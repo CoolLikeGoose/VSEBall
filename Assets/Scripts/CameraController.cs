9654e76f75b5b4e0e1b111692b67f6d6e584d02d
CoolLikeGoose <niken54@mail.ru>
2020-07-09 12:16:32 +0300

    Added camera movement, Updated project structure

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    [SerializeField] private float smoothFactor;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.position;
+    }   
+
+    private void Update()
+    {
+        //transform.position = Vector3.Lerp(transform.position, player.position + offset, smoothFactor);
+        transform.position = player.position + offset;
+    }
+}
