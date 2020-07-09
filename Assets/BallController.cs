ec658c72ed6c2ca03a547ed609c779020b04e8e7
CoolLikeGoose <niken54@mail.ru>
2020-07-09 11:58:19 +0300

    Added ball movement

---
+++ Assets/BallController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * speed;
+        rigidbody.AddForce(movement);
+    }
+}
