f4355f2665d4c0e2e25c539752ef918ea5980af5
CoolLikeGoose <niken54@mail.ru>
2020-07-09 18:44:17 +0300

    Updated ball control and camera

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,19 +1,39 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float speed;
 
-    private Rigidbody rigidbody;
+    private Rigidbody rbody;
+
+    [NonSerialized] public Vector3 startAcceleration;
+
+    public Vector3 movement;
 
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rbody = GetComponent<Rigidbody>();
+
+        Vector3 movement = new Vector3(Input.acceleration.x, Input.acceleration.z, Input.acceleration.y) * 9.8f;
+        rbody.AddForce(movement, ForceMode.Acceleration);
+
+        startAcceleration = Input.acceleration;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * speed;
-        rigidbody.AddForce(movement);
+        #if UNITY_EDITOR
+            movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * speed;
+            rbody.AddForce(movement);
+#elif UNITY_ANDROID
+            movement = new Vector3(Input.acceleration.x, Input.acceleration.z, Input.acceleration.y) - startAcceleration;
+            rbody.AddForce(movement * 9.8f, ForceMode.Acceleration);
+#endif
+    }
+
+    public void DiscardAcceleration()
+    {
+        startAcceleration = Input.acceleration;
     }
 }
---
+++ Assets/Scripts/Debug/DebugLog.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugLog : MonoBehaviour
+{
+    public Text Xaxis;
+    public Text Zaxis;
+    public Text Yaxis;
+    public Text startAxis;
+    public Text nowAxis;
+
+    public BallController ball;
+
+    private void Update()
+    {
+        Xaxis.text = $"X: {Input.acceleration.x}";
+        Zaxis.text = $"Z: {Input.acceleration.z}";
+        Yaxis.text = $"Y: {Input.acceleration.y}";
+        startAxis.text = $"start: {ball.startAcceleration}";
+        nowAxis.text = $"start: {ball.movement}";
+    }
+}
