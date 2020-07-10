e98df1b531cc5c80f61ece371921f071f681e746
CoolLikeGoose <niken54@mail.ru>
2020-07-10 19:22:28 +0300

    Added spinning platforms

---
+++ Assets/Scripts/SpiningPlatformController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SpiningPlatformController : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        transform.eulerAngles = new Vector3(Random.Range(-15, 15), 0, 0);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(rotationSpeed * Time.deltaTime, 0, 0));
+    }
+}
