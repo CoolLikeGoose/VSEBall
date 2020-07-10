a362327966d5013a308c9a6a80c3ba93f2ff9115
CoolLikeGoose <niken54@mail.ru>
2020-07-10 14:32:25 +0300

    Added lava flow

---
+++ Assets/Scripts/LavaController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class LavaController : MonoBehaviour
+{
+    /*
+    [SerializeField] private float startRadius;
+    [SerializeField] private float deathRadius;
+
+    [SerializeField] private Vector3 startCube;
+    [SerializeField] private Vector3 deathCube;
+
+    private void Start()
+    {
+        startCube = transform.localScale + new Vector3(2, 2, 2);    
+        deathCube = transform.localScale + new Vector3(7, 3, 7);
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawWireSphere(transform.position, startRadius);
+        //Gizmos.DrawWireSphere(transform.position, deathRadius);
+
+        Gizmos.DrawWireCube(transform.position, startCube);
+        Gizmos.DrawWireCube(transform.position, deathCube);
+    }
+    */
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<BallController>().nowLavaCenter = transform.position.x;  
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            BallController playerBall = other.GetComponent<BallController>();
+            if (playerBall.nowLavaCenter == transform.position.x)
+            {
+                playerBall.nowLavaCenter = 0;
+                playerBall.rend.material.color = playerBall.startColor;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/BallController.cs
+++ Assets/Scripts/Player/BallController.cs
@@ -6,15 +6,21 @@ public class BallController : MonoBehaviour
     [SerializeField] private float speed;
 
     private Rigidbody rbody;
+    [NonSerialized] public Renderer rend;
+    [NonSerialized] public Color startColor;
 
     [NonSerialized] public Vector3 startAcceleration;
     [NonSerialized] public Vector3 movement;
 
     private Vector3 startPos;
 
+    [NonSerialized] public float nowLavaCenter = 0;
+
     private void Start()
     {
         rbody = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
 
         startAcceleration = Input.acceleration;
         startPos = transform.position;
@@ -24,6 +30,16 @@ public class BallController : MonoBehaviour
             transform.position = startPos;
             rbody.velocity = Vector3.zero;
         };
+    }
+
+    //fix it
+    private void Update()
+    {
+        if (nowLavaCenter != 0) 
+        {
+            rend.material.color = Color.LerpUnclamped(startColor, Color.black, transform.position.x/nowLavaCenter * 1.3f);
+            if (transform.position.x / nowLavaCenter > 0.7f) { GameManager.OnRoundLose?.Invoke(); }
+        }
     }
 
     private void FixedUpdate()
